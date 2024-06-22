67307e8541f3249a21722c5a6f5a2c788ef971af
Ayush <ayush.sharma617@gmail.com>
2024-06-22 21:00:35 +0530

    Initial Commit

---
+++ src/@common/layout/app-header/app-header-tools/app-header-tools.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-header-tools',
+  templateUrl: './app-header-tools.component.html',
+  styleUrls: ['./app-header-tools.component.scss']
+})
+export class AppHeaderToolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
