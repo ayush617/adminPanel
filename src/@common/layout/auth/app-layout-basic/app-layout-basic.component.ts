aa16bb30c47ac73a275783dce9c12b0ba7480eb4
Ayush <ayush.sharma617@gmail.com>
2024-06-29 04:52:58 +0530

    greate login bg

--- src/@common/layout/auth/app-layout-basic/app-layout-basic.component.ts
+++ src/@common/layout/auth/app-layout-basic/app-layout-basic.component.ts
@@ -9,7 +9,23 @@ export class AppLayoutBasicComponent implements OnInit {
 
   constructor() { }
 
+  colorOptions = [
+    "app-bg-primary-o-40",
+    "app-bg-secondary-o-40",
+    "app-bg-warning-o-40",
+    "app-bg-danger-o-40",
+    "app-bg-success-o-40"
+  ]
+
+  color
+
   ngOnInit(): void {
+    this.color = this.getRandomItem(this.colorOptions)
   }
+
+  getRandomItem(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
 
 }
